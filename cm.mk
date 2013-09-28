4771ea76f07d81cf37f60c6d4cfc0afa90934169
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-09-28 17:58:22 +0200

    Bump to Alpha 3
    Change-Id: I9d590789cbd22f6b42dcabf7e0c17e41d4d565c7

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(LOCAL_PATH)/full_ariesve.mk)
 
 PRODUCT_RELEASE_NAME := ariesve
 
-CM_BUILDTYPE := dev_connection_team-alpha2
-CM_NUMERICAL_VERSION := 0.0.2
+CM_BUILDTYPE := dev_connection_team-alpha3
+CM_NUMERICAL_VERSION := 0.0.3
 
 # Setup device configuration
 PRODUCT_NAME := cm_ariesve
