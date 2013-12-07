1b7e9638c4f2f939dea2bbdbddcf181868baf095
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-12-07 11:46:51 +0100

    CameraHAL: Add gralloc includes

--- camerahal/Android.mk
+++ camerahal/Android.mk
@@ -26,6 +26,12 @@ LOCAL_SRC_FILES := cameraHAL.cpp \
 LOCAL_C_INCLUDES := frameworks/av/include
 LOCAL_C_INCLUDES += frameworks/native/include
 
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
+LOCAL_C_INCLUDES += hardware/qcom/display-caf/libgralloc
+else
+LOCAL_C_INCLUDES += hardware/qcom/display-legacy/libgralloc
+endif
+
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libbinder
 LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
 LOCAL_SHARED_LIBRARIES += libseccameraadaptor
